0e697c5d23ed77a185d7de928fcb9cfbcb425837
Mukul <mukulkumar627@gmail.com>
2021-04-08 13:25:45 +0530

    Bruteforce solution to the problem. Still very much lacking in things like socket etc. Not fast enough yet, but works.

--- routes.js
+++ routes.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
+const readline = require('readline');
+
 const routes = {
-    logs: (data, res) => {
-        //TODO: Entire logic for filtering out the required logs
-        res.end(JSON.stringify(data));
+    logs: async (data, res) => {
+        //filtering out the required logs
+        const { startDate, startTime, endDate, endTime } = data;
+        console.log(startDate, startTime, endDate, endTime)
+        const fileStream = fs.createReadStream('./example.txt');
+
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        rl.on('line', (line) => {
+            const date = line.substr(0,10);
+            const time = line.substr(11,8);
+            if(date >= startDate && time >= startTime && date <= endDate && time <= endTime) {
+                res.write(line+"\n");
+                console.log(date,time);
+            }
+        });
+        rl.on('close', () => {
+            res.end();
+        });
     },
-    notFound: (data,res) => {
+    notFound: (data, res) => {
         //If no route matches
         let payload = {
             message: "File Not Found",
