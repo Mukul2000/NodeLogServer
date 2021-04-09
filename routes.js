8e0e398eb8d6cc2b2f4417a265d712f220458f95
Mukul <mukulkumar627@gmail.com>
2021-04-09 19:52:20 +0530

    Add binary search for searching. Handle other cases such as invalid requests or missing parameters.

--- routes.js
+++ routes.js
@@ -1,30 +1,76 @@
-const {lower_bound, Rlower_bound} = require('./search');
+const fs = require('fs');
+const { lower_bound } = require('./search');
 
 const routes = {
     logs: async (data, res) => {
         // date format is YYYY-MM-DD
         // time format is hh:mm:ss
         // searchString is date + "T" + time
-        const {startDate, startTime, endDate, endTime} = data.queryString;
+        const { startDate, startTime, endDate, endTime } = data.queryString;
+
+        //all parameters required
+        if (startDate === undefined || startTime === undefined
+            || endDate === undefined || endTime === undefined) {
+            bad_request("Bad request, request parameters missing", res, 400);
+            return;
+        }
+
+        //invalid request interval
+        if ((endDate < startDate) || (endTime < startTime)) {
+            bad_request("Bad request, request intervals incorrect", res, 400);
+            return;
+        }
+
+        //form search strings
         const startSearchString = startDate + "T" + startTime;
         const endSearchString = endDate + "T" + endTime;
 
-        
-    },
-    notFound: (data, res) => {
-        //If no route matches
-        let payload = {
-            message: "File Not Found",
-            code: 404
-        };
-        let payloadStr = JSON.stringify(payload);
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.writeHead(404);
 
-        res.write(payloadStr);
-        res.end("\n");
-    }
+        const upper = lower_bound(startSearchString);
+        const startLine = upper.ans;
+        const startLineByte = upper.start_byte;
+
+        const lower = lower_bound(endSearchString);
+        const endLine = lower.ans;
+        const endLineByte = lower.start_byte + lower.len;
+
+        //Take care of when any one of start`Line or endLine are null
+        //user is possibly requesting logs at a future point of time
+        if(startLine == null || endLine == null) {
+            bad_request("Something went wrong, possibly your parameters include a point of time in the future",res, 400);
+            return;
+        }
+
+        //serve up everything from startLineByte to endLineByte
+        //TODO: subject to limitation on how many logs to transfer
+        let bytesToServe = endLineByte - startLineByte + 1;
+        if (endLine > endSearchString) bytesToServe--;
+        let buffer = Buffer.alloc(bytesToServe);
+        fd = fs.openSync('example.txt', 'r');
+        let ps = fs.readSync(fd, buffer, 0, buffer.length, startLineByte);
+        let result = buffer.slice(0, ps).toString();
+        res.write(result);
+        res.end();
+        fs.closeSync(fd);
+
+
+    },
+    notFound: (data, res) => bad_request("Invalid route", res, 404)
+}
+
+function bad_request(message, res, code) {
+    //If no route matches
+    let payload = {
+        message: message,
+        code: code
+    };
+    let payloadStr = JSON.stringify(payload);
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(code);
+
+    res.write(payloadStr);
+    res.end("\n");
 }
 
 module.exports = routes;
--- search.js
+++ search.js
@@ -6,7 +6,8 @@ const { EOL } = require('os');
 function lower_bound(searchString) {
     const file_name = 'example.txt';
     let ans = null;
-
+    let start_byte = null;
+    let len = 0;
 
     fd = fs.openSync(file_name, 'r');
 
@@ -17,18 +18,20 @@ function lower_bound(searchString) {
     while (lo < hi) {
 
         let mid = lo + Math.floor((hi - lo) / 2);
-
-        let pos = fs.readSync(fd, buffer, 0, buffer.length, mid); //read 258 bytes and fill my buffer
-        const cur = buffer.slice(0, pos).toString();
-        for (let i = 0; i < cur.length; i++) {
-            if (cur[i] === EOL) {
-                pos = i + 1;
-                break;
+        let pos = 0;
+        if (mid > 0) {
+            pos = fs.readSync(fd, buffer, 0, buffer.length, mid); //read 258 bytes and fill my buffer
+            const cur = buffer.slice(0, pos).toString();
+            for (let i = 0; i < cur.length; i++) {
+                if (cur[i] === EOL) {
+                    pos = i + 1;
+                    break;
+                }
             }
         }
 
-
-        //mid + pos should point at start of next line
+        //mid + pos now points at start of new line
+        let pos2 = pos;
         let currentLogLine = ""; //will store from mid + pos to mid + pos + 258 bytes
         pos = fs.readSync(fd, buffer, 0, buffer.length, mid + pos);
         let current = buffer.slice(0, pos).toString();
@@ -44,68 +47,23 @@ function lower_bound(searchString) {
         if (dt >= searchString) {
             hi = mid - 1;
             ans = currentLogLine;
+            start_byte = mid + pos2;
+            len = currentLogLine.length;
         }
         else {
             lo = mid + 1;
         }
     }
 
-    console.log(ans);
-    return ans;
-}
+    fs.closeSync(fd);
 
-
-//find last timestamp <= searchString
-function Rlower_bound(searchString) {
-    const file_name = 'example.txt';
-    let ans = null;
-
-
-    fd = fs.openSync(file_name, 'r');
-
-    let buffer = Buffer.alloc(258);
-
-    let lo = 0, hi = fs.statSync(file_name).size - 1;
-
-    while (lo < hi) {
-
-        let mid = lo + Math.floor((hi - lo) / 2);
-
-        let pos = fs.readSync(fd, buffer, 0, buffer.length, mid); //read 258 bytes and fill my buffer
-        const cur = buffer.slice(0, pos).toString();
-        for (let i = 0; i < cur.length; i++) {
-            if (cur[i] === EOL) {
-                pos = i + 1;
-                break;
-            }
-        }
-
-
-        //mid + pos should point at start of next line
-
-        let currentLogLine = ""; //will store from mid + pos to mid + pos + 258 bytes
-        pos = fs.readSync(fd, buffer, 0, buffer.length, mid + pos);
-        let current = buffer.slice(0, pos).toString();
-        for (let i = 0; i < current.length; i++) {
-            currentLogLine += current[i];
-            if (current[i] === EOL) break;
-            //only want upto the first line break
-        }
-
-        let dt = currentLogLine.substr(0, 19); //extract the date and time part from the log line //OK
-
-        //update bounds
-        if (dt <= searchString) {
-            lo = mid + 1;
-            ans = currentLogLine;
-        }
-        else {
-            hi = mid - 1;
-        }
+    return {
+        ans,
+        start_byte,
+        len
     }
-
-    console.log(ans);
-    return ans;
 }
 
-module.exports = {lower_bound, Rlower_bound};
+
+
+module.exports = { lower_bound };
