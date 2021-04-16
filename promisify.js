25cde8b790053af7b02b6b4dd60fe14bcce806d8
Mukul <mukulkumar627@gmail.com>
2021-04-16 10:48:15 +0530

    Promisify fs functions

---
+++ promisify.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const {promisify} = require('util');
+
+module.exports = {
+    open : promisify(fs.open),
+    close: promisify(fs.close),
+    read: promisify(fs.read),
+    stat: promisify(fs.stat)
+}
